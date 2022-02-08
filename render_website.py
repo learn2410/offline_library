e1624a925a21e8bda1d7345a2e103c604b5b75e5
learn2410 <learn2410@mail.ru>
2022-02-08 23:05:02 +0500

    Add paginator.

--- render_website.py
+++ render_website.py
@@ -1,6 +1,6 @@
+import glob
 import json
 import os
-
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
@@ -9,7 +9,10 @@ from more_itertools import chunked
 LIB_DIR = 'library'
 TEXTS_SUBDIR = 'books'
 IMAGES_SUBDIR = 'images'
-PAGES_SUBDIR='pages'
+PAGES_SUBDIR = 'pages'
+BOOKS_PER_PAGE = 10
+PAGINATOR_LEN = 11
+
 
 def load_json(json_path):
     if os.path.exists(json_path) and os.path.isfile(json_path):
@@ -19,8 +22,30 @@ def load_json(json_path):
         catalog = {}
     for book in catalog.values():
         for key in ['img_src', 'book_path']:
-            book.update({key:'../../'+ book[key].replace('\\', '/')})
+            book.update({key: '../../' + book[key].replace('\\', '/')})
     return catalog
+
+
+def make_paginator(page, max_page):
+    link_template = f'../../{LIB_DIR}/{PAGES_SUBDIR}/index#.html'
+    xxx = []
+    if max_page == 1:
+        return xxx
+    ll = min(max_page + 1, PAGINATOR_LEN)
+    if ll <= max_page:
+        xxx.append(['первая', ''] if page == 1 else ['первая', link_template.replace('#', '1')])
+        xxx.append(['<<', ''] if page <= 1 else ['<<', link_template.replace('#', str(page - 1))])
+    bound = [page - ll // 2, page - ll // 2 + ll - 1]
+    offset = 1 - bound[0] if bound[0] < 1 else max_page - bound[1] + 1 if bound[1] > max_page else 0
+    if offset != 0:
+        bound = [x + offset for x in bound]
+    for i in range(*bound):
+        xxx.append([str(i), '#'] if i == page else [str(i), link_template.replace('#', str(i))])
+    if ll <= max_page:
+        xxx.append(['>>', ''] if page >= max_page else ['>>', link_template.replace('#', str(page + 1))])
+        xxx.append(['последняя', ''] if page == max_page else ['последняя', link_template.replace('#', str(max_page))])
+    return xxx
+
 
 def on_reload():
     json_path = os.path.join(LIB_DIR, 'catalog.json')
@@ -30,18 +55,29 @@ def on_reload():
         autoescape=select_autoescape(['html', 'xml'])
     )
     template = env.get_template('template.html')
-    for page_num,books in enumerate(list(chunked(catalog.values(),20))):
-        rendered_page = template.render(catalog=chunked(books,2))
-        with open(f'{LIB_DIR}/{PAGES_SUBDIR}/index{page_num+1}.html', 'w', encoding="utf8") as file:
+    end_page = (len(catalog) - 1) // BOOKS_PER_PAGE + 1
+    paginator_template = f'../../{LIB_DIR}/{PAGES_SUBDIR}/index#.html'
+    for page_num, books in enumerate(list(chunked(catalog.values(), BOOKS_PER_PAGE)), 1):
+        paginator = make_paginator(page_num, end_page)
+        rendered_page = template.render(
+            catalog=chunked(books, 2),
+            end_page=end_page,
+            current_page=page_num,
+            paginator_template=paginator_template,
+            paginator=paginator
+        )
+        with open(f'{LIB_DIR}/{PAGES_SUBDIR}/index{page_num}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
 
 def main():
     os.makedirs(f'{LIB_DIR}/{PAGES_SUBDIR}', exist_ok=True)
+    for file in glob.glob(f'{LIB_DIR}/{PAGES_SUBDIR}/index*.html'):
+        os.remove(file)
     on_reload()
     server = Server()
     server.watch('template.html', on_reload)
-    server.serve()
+    server.serve(root=f'.', default_filename=f'./{LIB_DIR}/{PAGES_SUBDIR}/index1.html')
 
 
 if __name__ == '__main__':
